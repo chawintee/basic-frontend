6657e8887773d07dd08baa8e650c000c2ee98676
Chawintee <Chawinteetea@gmail.com>
2022-03-15 13:18:00 +0700

    [trail] don't use type module 1 import and error

--- nomodule0.js
+++ nomodule0.js
@@ -1,7 +1,8 @@
 let test = document.getElementById("test")
 console.log(test);
 
+import {name , lastName, nickName} from "./nomodule1"
 
-document.getElementById("name").innerHTML = "Chawintee"
-document.getElementById("lastname").innerHTML = "Muennarong"
-document.getElementById("nickname").innerHTML = "Tobtab"
+document.getElementById("name").innerHTML = name
+document.getElementById("lastname").innerHTML = lastName
+document.getElementById("nickname").innerHTML = nickName
